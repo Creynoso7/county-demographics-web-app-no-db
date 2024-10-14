c9fa90bc764d265bc322decfa05a27628d7dd612
Creynoso7 <creynoso62103@gmail.com>
2024-10-14 13:24:32 -0700

    added code

--- webapp.py
+++ webapp.py
@@ -20,6 +20,14 @@ def render_fact():
     fact = "In " + state + ", the county with the highest percentage of under 18 year olds is " + county + "."
     return render_template('home.html', state_options=states, funFact=fact)
     
+@app.route('/showFact2')
+def render_fact2():
+    states = get_state_options()
+    state = request.args.get('state')
+    county = county_most_Manufacturers_Shipments(state)
+    fact2 = "In " + state + ", the county with the highest percentage of manufacturers shipments is " + county + "."
+    return render_template('home.html', state_options=states, funFact2=fact2)
+ 
 def get_state_options():
     """Return the html code for the drop down menu.  Each option is a state abbreviation from the demographic data."""
     with open('demographics.json') as demographics_data:
@@ -43,6 +51,18 @@ def county_most_under_18(state):
         if c["State"] == state:
             if c["Age"]["Percent Under 18 Years"] > highest:
                 highest = c["Age"]["Percent Under 18 Years"]
+                county = c["County"]
+    return county
+
+def county_most_Manufacturers_Shipments(state):
+    with open('demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
+    highest=0
+    county = ""
+    for c in counties:
+        if c["State"] == state:
+            if c["Miscellaneous"]["Manufacturers Shipments"] > highest:
+                highest = c["Miscellaneous"]["Manufacturers Shipments"]
                 county = c["County"]
     return county
 
